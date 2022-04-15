a83e05204bda88c32eca36192021e99a44d0cfd5
imzzuu <su_bin0619@naver.com>
2022-04-15 21:55:21 +0900

    feat : Make main section, badges and add animation
    make interval animation and badge opacity animation

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+// mini nav 검색 창 애니메이션
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 
@@ -13,4 +14,33 @@ searchInputEl.addEventListener("focus", () => {
 searchInputEl.addEventListener("blur", () => {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
+});
+
+// 벳지 display 애니메이션
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+
+// 순차적 애니메이션
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7, //0.7, 1.4, 2.1, 2.7
+    opacity: 1,
+  });
 });
